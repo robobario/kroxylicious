e2d05d8a6640fea66c6a8de68609dd76fe5c9fe7
github-action-benchmark <github@users.noreply.github.com>
2023-04-11 05:01:39 +0000

    add kafka producer perf test Benchmark (customSmallerIsBetter) benchmark result for 633ffcf539e9d2384099b475974b95c6484c1062

---
+++ .benchmark/data.js
@@ -0,0 +1,45 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1681189299930,
+  "repoUrl": "https://github.com/robobario/kproxy",
+  "entries": {
+    "kafka producer perf test Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Robert Young",
+            "username": "robobario",
+            "email": "robeyoun@redhat.com"
+          },
+          "committer": {
+            "name": "Robert Young",
+            "username": "robobario",
+            "email": "robeyoun@redhat.com"
+          },
+          "id": "633ffcf539e9d2384099b475974b95c6484c1062",
+          "message": "Arbitrary change",
+          "timestamp": "2023-03-09T08:43:33Z",
+          "url": "https://github.com/robobario/kproxy/commit/633ffcf539e9d2384099b475974b95c6484c1062"
+        },
+        "date": 1681189298903,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "AVG Latency",
+            "value": 8.17,
+            "unit": "ms"
+          },
+          {
+            "name": "95th Latency",
+            "value": 4,
+            "unit": "ms"
+          },
+          {
+            "name": "99th Latency",
+            "value": 302,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
