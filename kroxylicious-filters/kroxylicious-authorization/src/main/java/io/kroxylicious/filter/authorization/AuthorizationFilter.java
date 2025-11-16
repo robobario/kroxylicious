750c841b9c10fe1e1c26661ef83017356db5f80a
Robert Young <robertyoungnz@gmail.com>
2025-11-17 12:01:12 +1300

    Enforce alter configs authorization
    User must be permitted to ALTER_CONFIGS for topic. If they aren't then
    we do not forward on the config alteration and augment in an error code
    on the response.
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/AuthorizationFilter.java
+++ kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/AuthorizationFilter.java
@@ -88,7 +88,9 @@ public class AuthorizationFilter implements RequestFilter, ResponseFilter {
         apiEnforcement.put(ApiKeys.ADD_OFFSETS_TO_TXN, new Passthrough<>(0, 4));
         apiEnforcement.put(ApiKeys.END_TXN, new Passthrough<>(0, 5));
 
+
         apiEnforcement.put(ApiKeys.DESCRIBE_CONFIGS, new DescribeConfigsEnforcement());
+        apiEnforcement.put(ApiKeys.ALTER_CONFIGS, new AlterConfigsEnforcement());
     }
 
     @VisibleForTesting
