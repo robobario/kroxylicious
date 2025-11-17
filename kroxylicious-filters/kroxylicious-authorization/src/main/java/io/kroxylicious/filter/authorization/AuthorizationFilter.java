936931a3ba4615ddd794167c8d89551b064dea03
Robert Young <robertyoungnz@gmail.com>
2025-11-17 13:44:22 +1300

    Enforce consumer group heartbeat authorization
    If a heartbeat attempts to subscribe to any topic that the user does not
    have DESCRIBE permissions for, then we short-circuit respond with an
    authorization error for the request as a whole.
    
    Signed-off-by: Robert Young <robertyoungnz@gmail.com>

--- kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/AuthorizationFilter.java
+++ kroxylicious-filters/kroxylicious-authorization/src/main/java/io/kroxylicious/filter/authorization/AuthorizationFilter.java
@@ -83,6 +83,7 @@ public class AuthorizationFilter implements RequestFilter, ResponseFilter {
         apiEnforcement.put(ApiKeys.FIND_COORDINATOR, new Passthrough<>(0, 6));
         apiEnforcement.put(ApiKeys.JOIN_GROUP, new Passthrough<>(0, 9));
         apiEnforcement.put(ApiKeys.SYNC_GROUP, new Passthrough<>(0, 5));
+        apiEnforcement.put(ApiKeys.CONSUMER_GROUP_HEARTBEAT, new ConsumerGroupHeartbeatEnforcement());
         apiEnforcement.put(ApiKeys.INIT_PRODUCER_ID, new Passthrough<>(0, 6));
         apiEnforcement.put(ApiKeys.ADD_PARTITIONS_TO_TXN, new AddPartitionsToTxnSingleTransactionEnforcement());
         apiEnforcement.put(ApiKeys.ADD_OFFSETS_TO_TXN, new Passthrough<>(0, 4));
